27d97ae353011559c6a557d112d5dc05958d21a9
EmpiresDesMachines <empiresdesmachines@gmail.com>
2025-12-05 09:01:25 +0300

    create router structure

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,35 @@ import { Provider } from "react-redux"
 import { App } from "./App"
 import { store } from "./app/store"
 import "./index.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { Main } from "./pages/Main"
+import { Profile } from "./pages/Profile"
+import { Product } from "./pages/Product"
+import { Category } from "./pages/Category"
+import { Products } from "./pages/Products"
+import { Register } from "./pages/Register"
+import { Login } from "./pages/Login"
+import { NotFound } from "./pages/NotFound"
+import { Layout } from "./components/Layout"
+import { UpdateProfileForm as UpdateProfile } from "./pages/UpdateProfile"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Main /> },
+      { path: "auth/register", element: <Register /> },
+      { path: "auth/login", element: <Login /> },
+      { path: "user/profile", element: <Profile /> },
+      { path: "user/profile/update", element: <UpdateProfile /> },
+      { path: "products/catalog/:id", element: <Product /> },
+      { path: "products/:category", element: <Category /> },
+      { path: "products", element: <Products /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+])
 
 const container = document.getElementById("root")
 
@@ -13,7 +42,7 @@ if (container) {
   root.render(
     <StrictMode>
       <Provider store={store}>
-        <App />
+        <RouterProvider router={router} />
       </Provider>
     </StrictMode>,
   )
